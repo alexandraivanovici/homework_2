81571fd80116b12ce58305b372f238427a906e4d
alexandraivanovici <ivanoviciioana18@stud.ase.ro>
2020-11-29 22:06:19 +0200

    tema 2

--- main/app.js
+++ main/app.js
@@ -1,5 +1,26 @@
-function addTokens(input, tokens){
-    
+function addTokens(input, tokens) {
+    if (typeof input !== 'string') {
+        throw Error("Invalid input")
+    }
+
+    if (input.length < 6) {
+        throw Error("Input should have at least 6 characters")
+    }
+
+    if (!tokens[0].hasOwnProperty('tokenName') || typeof tokens[0].tokenName !== 'string') {
+        throw Error("Invalid array format")
+    }
+
+    if (!input.includes("...")) {
+        return input;
+    }
+
+    let j = 0
+    while (input.includes("...")) {
+        input = input.replace('...', '${' + tokens[j].tokenName + '}')
+        j++
+    }
+    return input;
 }
 
 const app = {
